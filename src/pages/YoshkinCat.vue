ea8ea06f03edc98181f4d3e71ab8b0968634843f
Elena Frolova <frolova.est@gmail.com>
2024-12-19 16:05:38 +0700

    первый экран

--- src/pages/YoshkinCat.vue
+++ src/pages/YoshkinCat.vue
@@ -1,42 +1,49 @@
 <template>
   <q-page>
-        <div class="absolute-top">
-          <q-img src='src/assets/bg-cat.png'>
-            <div class="absolute-full text-subtitle2 flex flex-left items-center fix">
-              <div>
-                <h1>Экскурсии с Йошкиным котом по Красному городу</h1>
-                <q-img src='src/assets/заголовок.svg' class="dim"></q-img>
-                <p class="text-body1"> Текст — зафиксированная на каком-либо материальном носителе человеческая мысль</p>
-              </div>
-            </div>
-          </q-img>
-    </div>
-
-  </q-page>
-  <!-- <q-btn-group>
-      <q-btn outline rounded class="q-mr-md" label="Интерактивность" color="primary" />
-      <q-btn outline rounded class="q-mr-md" label="Интерактивность" color="primary" />
-      <q-btn outline rounded class="q-mr-md" label="Инклюзивность" color="primary" />
-      <q-btn outline rounded class="q-mr-md" label="Образованность" color="primary" />
-    </q-btn-group> -->
+      <q-img src='src/assets/bg-yct.png'>
+        <div class="absolute-full text-subtitle2 flex flex-left items-center fix">
+          <div>
+            <h1>Экскурсии с Йошкиным котом по Красному городу</h1>
+            <q-img src='src/assets/заголовок.svg' class="dim"></q-img>
+            <p class="text-body1"> История и современность: инклюзивное путешествие по Йошкар-Оле.</p>
+          </div>
+        </div>
+      </q-img>
     <div class="marquee">
     <div class="marquee-content">
-      <img src="src/assets/image1_btn.png" alt="Image 1" class="marquee-image" />
-      <img src="src/assets/image1_btn.png" alt="Image 2" class="marquee-image" />
-      <img src="src/assets/image1_btn.png" alt="Image 3" class="marquee-image" />
-      <img src="src/assets/image1_btn.png" alt="Image 4" class="marquee-image" />
-      <img src="src/assets/image1_btn.png" alt="Image 1" class="marquee-image" />
-      <img src="src/assets/image1_btn.png" alt="Image 2" class="marquee-image" />
-      <img src="src/assets/image1_btn.png" alt="Image 3" class="marquee-image" />
-      <img src="src/assets/image1_btn.png" alt="Image 4" class="marquee-image" />
+      <q-img src="src/assets/image1_btn.png" alt="Image 1" class="marquee-image">
+        <div class="text-overlay">Интерактивность </div>
+      </q-img>
+      <q-img src="src/assets/image1_btn.png" alt="Image 2" class="marquee-image">
+        <div class="text-overlay">Доступность </div>
+      </q-img>
+      <q-img src="src/assets/image1_btn.png" alt="Image 3" class="marquee-image">
+        <div class="text-overlay">Инклюзивность </div>
+      </q-img>
+      <q-img src="src/assets/image1_btn.png" alt="Image 4" class="marquee-image">
+        <div class="text-overlay">Образованность </div>
+      </q-img>
+      <q-img src="src/assets/image1_btn.png" alt="Image 1" class="marquee-image">
+        <div class="text-overlay">Интерактивность </div>
+      </q-img>
+      <q-img src="src/assets/image1_btn.png" alt="Image 2" class="marquee-image">
+        <div class="text-overlay">Доступность </div>
+      </q-img>
+      <q-img src="src/assets/image1_btn.png" alt="Image 3" class="marquee-image">
+        <div class="text-overlay">Инклюзивность </div>
+      </q-img>
+      <q-img src="src/assets/image1_btn.png" alt="Image 4" class="marquee-image">
+        <div class="text-overlay">Образованность </div>
+      </q-img>
     </div>
   </div>
+  </q-page>
 </template>
 
 <script setup>
 //
 </script>
-<style lang="scss">
+<style scoped lang="scss">
 h1 {
   width: 1172px;
   font-size: 70px;
@@ -46,15 +53,21 @@ h1 {
 .text-body1 {
   font-size: 40px;
   font-weight: 600;
-  max-width:55vw ;
-  width: 900px;
-  color: #091A39;
+  max-width:59vw ;
+  width: 1022px;
+  color: #101011;
+  font-family: Montserrat Alternates;
+  line-height: 50px;
+  text-align: left;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+  padding-top: 10px;
 }
 .fix {
   background-color: transparent !important;
 }   
 .dim {
-  width: 59vw;
+  width: 53vw;
   height: auto;
 }
 .marquee {
@@ -62,20 +75,36 @@ h1 {
   white-space: nowrap;
   box-sizing: border-box;
   width: 100%; /* Ширина области бегущей строки */
+  padding-top: 22px;
 }
 
 .marquee-content {
   display: inline-block;
-  animation: marquee 40s linear infinite; /* Анимация */
-}
-.marquee-inner {
-  display: inline-block; /* Обертываем изображения в дополнительный div */
-}
-.marquee-image {
-  height: 100px;
-  margin-right: 16px;
+  animation: marquee 40s linear infinite;
 }
 
+.marquee-image {
+  height: 79px;
+  width: 527px;
+  margin-right: 26px;
+}
+.text-overlay {
+  position: absolute; /* Абсолютное позиционирование для текста */
+  top: 50%; /* Центрируем по вертикали */
+  left: 50%; /* Центрируем по горизонтали */
+  transform: translate(-50%, -50%); /* Сдвигаем текст на половину его ширины и высоты */
+  color: rgb(33, 31, 31);
+  font-family: Montserrat Alternates;
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 65px;
+  letter-spacing: -0.92px;
+  text-underline-position: from-font;
+  text-decoration-skip-ink: none;
+ 
+  text-align: center; /* Центрируем текст */
+  background-color: transparent;
+}
 @keyframes marquee {
   0% {
     transform: translateX(0); /* Начальная позиция (слева) */
